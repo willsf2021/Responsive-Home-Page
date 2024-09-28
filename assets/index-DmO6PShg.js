52b7c4172930ecea562148b48b20cfab7f64df35
willsf2021 <willsf2021@gmail.com>
2024-09-28 12:42:41 -0300

    Updates

--- assets/index-DmO6PShg.js
+++ assets/index-DmO6PShg.js
@@ -43,11 +43,13 @@ Error generating stack: `+o.message+`
       margin: 0;
       padding: 0;
       box-sizing: border-box;
+      overflow-x: hidden;
     }
 
     body {
       max-width: 375px;
       margin: auto;
+     
     }
 `,q={primary:{lightRed:"hsl(356, 100%, 66%)",veryLightRed:"hsl(355, 100%, 74%)",veryDarkBlue:"hsl(208, 49%, 24%)"},neutral:{grayishBlue:"hsl(240, 2%, 79%)",grayishBlueMobileMenu:"hsla(240, 3%, 79%, 0.4)",veryDarkGrayishBlue:"hsl(207, 13%, 34%)",veryDarkBlackBlue:"hsl(240, 10%, 16%)"},gradient:{veryLightRed:"hsl(13, 100%, 72%)",lightRed:"hsl(353, 100%, 62%)"},gradientBody:{veryDarkGrayBlue:"hsl(237, 17%, 21%)",veryDarkDesaturatedBlue:"hsl(237, 23%, 32%)"}},B1=te.header`
   position: relative;
